56306df22c4c2eb9bb4aa67d58b7c317aa9ffa74
Julien Houyet <jhouyet@shi-r6-p7.s19.be>
2023-10-27 11:28:22 +0200

    Protect ft_memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: jhouyet <jhouyet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/20 09:42:47 by jhouyet           #+#    #+#             */
-/*   Updated: 2023/10/27 10:39:56 by jhouyet          ###   ########.fr       */
+/*   Updated: 2023/10/27 11:26:58 by jhouyet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	char	*s;
 	size_t	i;
 
+	if (dst == NULL && src == NULL)
+		return (0);
 	d = (char *)dst;
 	s = (char *)src;
 	i = 0;
