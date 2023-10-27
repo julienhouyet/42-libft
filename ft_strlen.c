fe56b7e61ad0c0ae1e6e5d30119aa73130bd34e1
Julien Houyet <jhouyet@shi-r6-p7.s19.be>
2023-10-27 10:55:35 +0200

    Protect ft_strlen

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: jhouyet <jhouyet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/19 11:54:04 by jhouyet           #+#    #+#             */
-/*   Updated: 2023/10/27 10:36:22 by jhouyet          ###   ########.fr       */
+/*   Updated: 2023/10/27 10:53:53 by jhouyet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ size_t	ft_strlen(const char *s)
 {
 	int	i;
 
+	if (s == NULL)
+		return (0);
 	i = 0;
 	while (s[i] != '\0')
 	{
