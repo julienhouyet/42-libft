1441df246e193f242bd22f89833572fb85e617d1
Julien Houyet <julien.houyet@teradev.be>
2023-10-28 14:37:51 +0200

    Update ft_strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: jhouyet <jhouyet@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/20 14:49:32 by jhouyet           #+#    #+#             */
-/*   Updated: 2023/10/28 14:24:47 by jhouyet          ###   ########.fr       */
+/*   Updated: 2023/10/28 14:36:15 by jhouyet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,12 @@ char	*ft_strtrim(char const *s1, char const *set)
 	char	*str;
 
 	i = 0;
-	j = ft_strlen(s1);
 	if (s1 == NULL || set == NULL)
 		return (0);
+	j = ft_strlen(s1);
 	while (s1[i] && ft_strchr(set, s1[i]))
 		i++;
-	while (s1[j - 1] && ft_strchr(set, s1[j - 1]) && j > i)
+	while (j > i && s1[j - 1] && ft_strchr(set, s1[j - 1]))
 		j--;
 	str = (char *)malloc(sizeof(char) * (j - i + 1));
 	if (str != NULL)
