4d4033cda34aa36e226e6aaf45ff6835adbdb444
Julien Houyet <julien.houyet@teradev.be>
2023-10-28 08:58:44 +0200

    Update ft_strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -19,7 +19,7 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 
 	i = 0;
 	j = 0;
-	if (needle == NULL || needle[0] == '\0')
+	if (*needle == '\0')
 		return ((char *)haystack);
 	while (haystack[i] != '\0' && i < len)
 	{
