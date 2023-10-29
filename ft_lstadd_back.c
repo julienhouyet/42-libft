a42edf81432a7dfdd4fca7a71877d71d6e0fad93
Julien Houyet <julien.houyet@teradev.be>
2023-10-29 09:27:12 +0100

    Add ft_lstadd_back

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,8 +6,16 @@
 /*   By: jhouyet <jhouyet@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/28 16:38:43 by jhouyet           #+#    #+#             */
-/*   Updated: 2023/10/28 16:38:55 by jhouyet          ###   ########.fr       */
+/*   Updated: 2023/10/29 09:26:42 by jhouyet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	if (*lst)
+		ft_lstlast(*lst)->next = new;
+	else
+		*lst = new;
+}
