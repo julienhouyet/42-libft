6782b7e0f3ca35db97c4c5f0352ef00831325083
Julien Houyet <julien.houyet@teradev.be>
2023-10-29 11:24:42 +0100

    Add ft_lstmap

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,8 +6,28 @@
 /*   By: jhouyet <jhouyet@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/28 16:40:11 by jhouyet           #+#    #+#             */
-/*   Updated: 2023/10/28 16:40:25 by jhouyet          ###   ########.fr       */
+/*   Updated: 2023/10/29 11:21:44 by jhouyet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*new_list;
+	t_list	*changed_elem;
+
+	new_list = NULL;
+	while (lst)
+	{
+		changed_elem = ft_lstnew(f(lst->content));
+		if (!changed_elem)
+		{
+			ft_lstclear(&new_list, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&new_list, changed_elem);
+		lst = lst->next;
+	}
+	return (new_list);
+}
