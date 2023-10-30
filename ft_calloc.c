c7b4956ac4fd688327411f2c61933e178e8849ca
Julien Houyet <jhouyet@shi-r6-p7.s19.be>
2023-10-30 08:27:10 +0100

    Update ft_calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: jhouyet <jhouyet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/25 08:52:49 by jhouyet           #+#    #+#             */
-/*   Updated: 2023/10/27 11:38:12 by jhouyet          ###   ########.fr       */
+/*   Updated: 2023/10/30 08:26:56 by jhouyet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,6 @@ void	*ft_calloc(size_t count, size_t size)
 	void	*b;
 	size_t	s;
 
-	if ((count * size / size) != count)
-		return (0);
 	s = (count * size);
 	b = malloc(count * size);
 	if (b != NULL)
