db47fd6b82aab9e48dbd7ca4ab6247d43105ace1
Julien Houyet <julien.houyet@teradev.be>
2023-10-31 18:53:38 +0100

    Protect ft_atoi against long long int bug

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,17 +6,29 @@
 /*   By: jhouyet <jhouyet@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/25 09:06:48 by jhouyet           #+#    #+#             */
-/*   Updated: 2023/10/28 14:14:59 by jhouyet          ###   ########.fr       */
+/*   Updated: 2023/10/31 18:50:34 by jhouyet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
+
+static long long int	ft_secure(long long int result, long long int temp, \
+int neg)
+{
+	if (temp > result && neg == -1)
+		return (0);
+	else if (temp > result && neg == 1)
+		return (-1);
+	return (result);
+}
 
 int	ft_atoi(const char *str)
 {
-	int			i;
-	int			neg;
-	long int	result;
+	int				i;
+	int				neg;
+	long int		result;
+	long long int	temp;
 
 	i = 0;
 	neg = 1;
@@ -31,8 +43,10 @@ int	ft_atoi(const char *str)
 	}
 	while (str[i] >= '0' && str[i] <= '9')
 	{
+		temp = result;
 		result = (result * 10) + (str[i] - '0');
+		result = ft_secure(result, temp, neg);
 		i++;
 	}
-	return (result * neg);
+	return ((int)result * neg);
 }
