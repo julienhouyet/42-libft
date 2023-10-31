10e98abac9c24d99dd6137ecc7b62d9a82443e9b
Julien Houyet <jhouyet@shi-r6-p7.s19.be>
2023-10-31 15:18:18 +0100

    Edit ft_substr protect

--- ft_substr.c
+++ ft_substr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_substr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jhouyet <jhouyet@student.s19.be>           +#+  +:+       +#+        */
+/*   By: jhouyet <jhouyet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/20 11:15:59 by jhouyet           #+#    #+#             */
-/*   Updated: 2023/10/28 07:41:30 by jhouyet          ###   ########.fr       */
+/*   Updated: 2023/10/31 15:10:47 by jhouyet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,12 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	size_t	i;
 	size_t	j;
 
+	if (!s)
+		return (0);
 	if (ft_strlen(s) < len)
 		len = ft_strlen(s);
 	new_str = (char *)malloc(sizeof(*s) * (len +1));
-	if (!new_str || !s)
+	if (!new_str)
 		return (0);
 	i = 0;
 	j = 0;
