1af639433479f9cc0da9aa7861914d7ed8d39f77
Julien Houyet <jhouyet@shi-r6-p7.s19.be>
2023-10-31 15:09:53 +0100

    Minor change

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstmap_bonus.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jhouyet <jhouyet@student.s19.be>           +#+  +:+       +#+        */
+/*   By: jhouyet <jhouyet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/28 16:40:11 by jhouyet           #+#    #+#             */
-/*   Updated: 2023/10/30 13:47:09 by jhouyet          ###   ########.fr       */
+/*   Updated: 2023/10/31 09:54:14 by jhouyet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,20 @@
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new_list;
-	t_list	*changed_elem;
+	t_list	*new_elem;
 
 	if (!lst || !f)
 		return (NULL);
 	new_list = NULL;
 	while (lst)
 	{
-		changed_elem = ft_lstnew(f(lst->content));
-		if (!changed_elem)
+		new_elem = ft_lstnew(f(lst->content));
+		if (!new_elem)
 		{
 			ft_lstclear(&new_list, del);
 			return (NULL);
 		}
-		ft_lstadd_back(&new_list, changed_elem);
+		ft_lstadd_back(&new_list, new_elem);
 		lst = lst->next;
 	}
 	return (new_list);
